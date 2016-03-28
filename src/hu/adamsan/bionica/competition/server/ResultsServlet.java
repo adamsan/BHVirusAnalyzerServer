083e175fbd7d3304ce1799ff3da0574d8ea248d8
Adam Szentrimai-Schon <adamsan3@gmail.com>
2016-03-28 14:45:17 +0200

    code formatting

--- src/hu/adamsan/bionica/competition/server/ResultsServlet.java
+++ src/hu/adamsan/bionica/competition/server/ResultsServlet.java
@@ -1,6 +1,7 @@
 package hu.adamsan.bionica.competition.server;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Random;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import hu.adamsan.bionica.competition.server.model.CompetitionResult;
 import hu.adamsan.bionica.competition.server.service.CompetitionResultService;
 
 /**
@@ -17,11 +19,11 @@ import hu.adamsan.bionica.competition.server.service.CompetitionResultService;
 @WebServlet(urlPatterns = { "/results", "/index", "/index.html", "/index.jsp" })
 public class ResultsServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
     // To run this without tomcat:
     // Build webapp-runner:
     // mvn -Dmaven.test.skip=true package
@@ -29,10 +31,11 @@ public class ResultsServlet extends HttpServlet {
     // mvn clean build package
     // copy the jar and the war file to the same location, and run:
     // java -jar webapp-runner.jar --port 80 BHVirusAnalyzerServer-0.0.1-SNAPSHOT.war
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setAttribute("hello", "Hello everybody " + new Random().nextInt(100));
-        request.setAttribute("resultSize", CompetitionResultService.INSTANCE.getAllResults().size());
-        request.setAttribute("results", CompetitionResultService.INSTANCE.getAllResults());
+        List<CompetitionResult> results = CompetitionResultService.INSTANCE.getAllResults();
+        request.setAttribute("resultSize", results.size());
+        request.setAttribute("results", results);
         getServletContext().getRequestDispatcher("/WEB-INF/results.jsp").forward(request, response);
-	}
+    }
 }
