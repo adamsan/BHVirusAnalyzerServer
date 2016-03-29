d0c81fdb61d803c16db991e8a129d6de1f164630
Adam Szentrimai-Schon <adamsan3@gmail.com>
2016-03-29 13:58:18 +0200

    fix class name mistype

--- src/hu/adamsan/bionica/competition/server/service/EnvironmentContext.java
+++ src/hu/adamsan/bionica/competition/server/service/EnvironmentContext.java
@@ -1,6 +1,6 @@
 package hu.adamsan.bionica.competition.server.service;
 
-import hu.adamsan.bionica.competition.server.dao.CompetitioinResultArrayListDao;
+import hu.adamsan.bionica.competition.server.dao.CompetitionResultArrayListDao;
 import hu.adamsan.bionica.competition.server.dao.DAO;
 import hu.adamsan.bionica.competition.server.model.CompetitionResult;
 
@@ -8,7 +8,7 @@ public class EnvironmentContext {
 
     public static DAO<CompetitionResult> getCompetitionResultDAO() {
         // TODO: give different DAOs back based on the environment
-        return CompetitioinResultArrayListDao.INSTANCE;
+        return CompetitionResultArrayListDao.INSTANCE;
     }
 
 }
